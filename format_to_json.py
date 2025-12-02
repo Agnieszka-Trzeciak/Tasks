d64c1cfe0c2d43d9a1b17639ab04195bc3e5e98c
Agnieszka-Trzeciak <trzeciakagnieszka98@gmail.com>
2025-12-02 19:25:48 +0100

    Delete format_to_json.py

--- format_to_json.py
+++
@@ -1,9 +0,0 @@
-import re
-
-with open('task1_d.json') as f:
-    contents = f.readlines()[0] # Read the file in
-    contents = re.sub(r':([\w|\[|\]]+)\=\>',r'"\1":',contents) # Change all the :NAME=> into "NAME":
-    contents = re.sub(r'}, ',r'},\n',contents) # Add end of lines for better readability
-    with open('task1_d_processed.json', 'w') as output: # Save processed file
-        output.write(contents)
-
